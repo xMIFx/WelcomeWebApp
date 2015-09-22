c68ec5d06fc60efba963a975b378808e0e42118d
Vladislav <bukatinvladislav@gmail.com>
2015-09-22 18:19:49 +0300

    need locale browser

--- src/main/java/com/gitHub/xMIFx/welcomeWebApp/client/LoginServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package com.gitHub.xMIFx.welcomeWebApp.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface LoginServiceAsync {
-    void authorization(String login, String password, AsyncCallback<Boolean> async);
-}
